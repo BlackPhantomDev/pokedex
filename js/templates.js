ecf1274d0072eaac3a4aadd69fda143c5d57ab2f
M. Siegrist <therealphantom22@proton.me>
2026-01-27 14:19:29 +0100

    moved card to template

---
+++ js/templates.js
@@ -0,0 +1,31 @@
+function getPokemonCardTemplate(name, stats, types, weight, img, headerColor) {
+    return `
+        <div class="pokemon-card">
+            <div class="card-header ${headerColor}" id="pokemon-card-name">
+                ${name}
+            </div>
+            <div class="card-body">
+                <div class="card-image">
+                    <img id="pokemon-card-img" src="${img}" alt="Pokemon">
+                </div>
+                <div class="card-stats">
+                    <div class="stat">
+                        <div class="stat-value">${stats[1]["base_stat"]}</div>
+                        <div class="stat-label">Attack</div>
+                    </div>
+                    <div class="stat">
+                        <div class="stat-value">${stats[2]["base_stat"]}</div>
+                        <div class="stat-label">Defence</div>
+                    </div>
+                    <div class="stat">
+                        <div class="stat-value">${stats[5]["base_stat"]}</div>
+                        <div class="stat-label">Speed</div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+                Type: ${types} | HP: ${stats[0]["base_stat"]}
+            </div>
+        </div>
+    `;
+}
